23a0fe05b74602e0ee83b9f647b230321d759f17
Paul Sisson <paulsissonsemail@gmail.com>
2023-11-28 17:06:10 -0600

    Variable types/typing

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+int myNum = 3;
+
+Console.WriteLine(myNum);
+
+double myPi = 3.14;
+
+Console.WriteLine(myPi);
+
+string myStr = "3";
+
+Console.WriteLine(myStr);
+
+bool myTF = true;
+
+Console.WriteLine(myTF);
+
+
